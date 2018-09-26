453689a77904cb484b7dae95d9bd557a0ad8595f
risswyck <risswyck@gmx.de>
2018-09-26 10:07:49 +0200

    adding POJOs. Closes #2.

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -0,0 +1,22 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Person {
+    private String firstName;
+    private String lasString;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLasString() {
+        return lasString;
+    }
+
+    public void setLasString(String lasString) {
+        this.lasString = lasString;
+    }
+}
