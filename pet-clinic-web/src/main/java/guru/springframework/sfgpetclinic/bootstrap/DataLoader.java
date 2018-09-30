3f47422a07301485bb4b0dc206d496040ba14489
risswyck <risswyck@gmx.de>
2018-09-30 19:09:58 +0200

    added PetTypes to bootstrap. Closes #32

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package guru.springframework.sfgpetclinic.bootstrap;
 
 import guru.springframework.sfgpetclinic.model.Owner;
+import guru.springframework.sfgpetclinic.model.PetType;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
+import guru.springframework.sfgpetclinic.services.PetTypeService;
 import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,19 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType savedDogType = getPetType("Dog");
+        PetType savedCatType = getPetType("Cat");
+
         createOwner("Michael", "Weston");
         createOwner("Fiona", "Glenanne");
         System.out.println("Loaded owners...");
@@ -27,6 +34,12 @@ public class DataLoader implements CommandLineRunner {
         createVet("Sam", "Axe");
         createVet("Jessie", "Porter");
         System.out.println("Loaded vets...");
+    }
+
+    private PetType getPetType(String name) {
+        PetType dog = new PetType();
+        dog.setName(name);
+        return petTypeService.save(dog);
     }
 
     private void createOwner(String firstName, String lastName) {
